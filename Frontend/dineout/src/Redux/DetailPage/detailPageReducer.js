b36b8d21845c1aa768a72b3dabefbfa09111b99b
ranjithkumark8 <ranjithkumark8@gmail.com>
2021-03-10 15:32:10 +0530

    detailPage

---
+++ Frontend/dineout/src/Redux/DetailPage/detailPageReducer.js
@@ -0,0 +1,60 @@
+import {
+  GET_RESTAURANT_FAILURE,
+  GET_RESTAURANT_REQUEST,
+  GET_RESTAURANT_SUCCESS,
+} from "./detailPageActions";
+
+const initialData = {
+  restaurantData: [],
+  isLoading: true,
+  isError: false,
+};
+
+export const restaurantDetailReducer = (
+  state = initialData,
+  { type, payload }
+) => {
+  switch (type) {
+    case GET_RESTAURANT_REQUEST: {
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    }
+    case GET_RESTAURANT_SUCCESS: {
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        restaurantData: payload,
+      };
+    }
+    case GET_RESTAURANT_FAILURE: {
+      return {
+        ...state,
+        isError: true,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+// restaurant_name: "",
+//         image: "",
+//         location: "",
+//         timings: "",
+//         girf: [],
+//         dineoutPay: "",
+//         passport: "",
+//         free_offer: [],
+//         menu_images: [],
+//         about: "",
+//         cuisine: "",
+//         type: "",
+//         average_cost: "",
+//         best_selling_items: "",
+//         facilities: [],
+//           contact: [],
+//         rating:""
