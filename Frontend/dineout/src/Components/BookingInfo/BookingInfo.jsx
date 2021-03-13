b8ff412979ae7e6bbc96ff912771e339bab9558a
gopichandana <gopichandanasiri@gmail.com>
2021-03-13 08:30:48 +0530

    Booking details

--- Frontend/dineout/src/Components/BookingInfo/BookingInfo.jsx
+++ Frontend/dineout/src/Components/BookingInfo/BookingInfo.jsx
@@ -2,33 +2,54 @@ import axios from "axios"
 import React from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getusers } from "../../Redux/User/actionTypes"
-
-
+import "./BookingInfo.css"
+import {v4 as uuid} from "uuid"
 const BookingInfo = ()=>{
-    
-    // const details = useSelector((store)=>store.loginDetails.loginData)
-    // const userdetails = useSelector((store)=>store.userDetails.userData)
-    // let userId=""
-    // for(let key in userdetails)
-    // {
-    //     if(userdetails[key].googleId===details.googleId)
-    //     {
-    //         userId = userdetails[key]._id
-    //     }
-    //     else{
-    //         console.log(userdetails[key].name)
-    //     }
-    // }
-    // console.log(userdetails)
     React.useEffect(()=>{
       return axios.get("http://localhost:6878/booking")
       .then((res)=>console.log(res))
     },[])
     return(
-        <div style={{marginTop:"150px"}}>
-            <div>
-                {}
-            </div>
+        <div style={{marginTop:"130px",marginBottom:"30px"}}>
+            {/* mapping */}
+            <div style={{width:"50%",margin:"auto",height:"600px",marginTop:"10px",marginBottom:"30px"}}>
+                <div className="rest_name">
+                  vbgfbnchfgh
+                </div>
+                <div className="details_box1">
+                    <div style={{fontSize:"28px"}}>Guest Details</div><br/>
+                    <div style={{display:"flex"}}>
+                    <div style={{display:"flex",flexDirection:"column",width:"100%"}}>
+                      <p>Guest Name</p>
+                      <p>Phone No</p>
+                      <p>Email Id</p>
+                    </div>
+                    <div style={{display:"flex",flexDirection:"column",width:"100%"}}>
+                    <p>1</p>
+                      <p>2</p>
+                      <p>3</p>
+                    </div>
+                    </div>
+                </div>
+                <div className="details_box2">
+                    <div style={{fontSize:"28px"}}>Booking Summary</div><br/>
+                    <div style={{display:"flex"}}>
+                    <div style={{display:"flex",flexDirection:"column",width:"100%"}}>
+                    <p>Table Status - Confirmed</p>
+                      <p>Date & Time</p>
+                      <p>Guest Count</p>
+                      <p>Booking Id</p>
+                    </div>
+                    <div style={{display:"flex",flexDirection:"column",width:"100%"}}>
+                    <p>1</p>
+                      <p>2</p>
+                    <p>3</p>
+                    <p>{uuid()}</p>
+                    </div>
+                    </div>
+                </div>
+            </div><br/>
+           
         </div>
     )
 }
